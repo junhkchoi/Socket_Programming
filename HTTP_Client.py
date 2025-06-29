1fd0364a91ba281c5cb9bfb14035cbc943ba060a
junhkchoi <leisure1566@gmail.com>
2025-06-29 12:16:44 +0900

    [Feat] CS환경 구현

--- HTTP_Client.py
+++ HTTP_Client.py
@@ -1,12 +1,16 @@
 from socket import *
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(('121.0.0.1', 8080)) # 클라이언트에서 서버로 접속 (자신의 IP, PORT)
+# 1. 클라이언트 소켓 생성 (IPv4, TCP)
+clientSock = socket(AF_INET, SOCK_STREAM)
 
-print('연결 확인됐습니다.')
-clientSocket.send('I am a client'.encode('utf-8'))
+# 2. 서버에 연결 요청
+clientSock.connect(('127.0.0.1', 8080))  # 또는 'localhost'
+print('연결 확인 됐습니다.')
 
-print('클라이언트가 메세지를 전송했습니다')
+# 3. 서버에 메시지 전송
+clientSock.send('I am a client'.encode('utf-8'))
+print('메시지를 전송했습니다.')
 
-data = clientSocket.recv(1024)
-print('받은 데이터: ', data.decode('utf-8'))
+# 4. 서버로부터 메시지 수신
+data = clientSock.recv(1024)
+print('받은 데이터 : ', data.decode('utf-8'))
--- HTTP_Server.py
+++ HTTP_Server.py
@@ -1,9 +1,22 @@
 from socket import *
 
-serverSocket = socket(AF_INET, SOCK_STREAM) # IPv4, TCP 방식
-serverSocket.bind(('', 8080)) # 소켓과 AF를 연결 (IP, PORT)
-serverSocket.listen(1) # 1개의 접속 허용
+# 1. 서버 소켓 생성 (IPv4, TCP)
+serverSock = socket(AF_INET, SOCK_STREAM)
 
-connectionSocket, addr = serverSocket.accept() # 접속 대기 후 수락
+# 2. 빈 문자열 ''은 모든 IP에서 접속을 허용, 8080번 포트로 바인딩
+serverSock.bind(('', 8080))
 
+# 3. 연결 대기 (최대 1개까지 연결 대기)
+serverSock.listen(1)
+
+# 4. 클라이언트 연결 수락
+connectionSock, addr = serverSock.accept()
 print(str(addr), '에서 접속이 확인되었습니다.')
+
+# 5. 클라이언트가 보낸 데이터 수신 (1024바이트까지)
+data = connectionSock.recv(1024)
+print('받은 데이터 : ', data.decode('utf-8'))
+
+# 6. 클라이언트에게 응답 전송
+connectionSock.send('I am a server.'.encode('utf-8'))
+print('메시지를 보냈습니다.')
